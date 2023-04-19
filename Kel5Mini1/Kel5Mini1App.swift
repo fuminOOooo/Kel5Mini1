a7492b7de7d4f845a716f686c24a2d4eb02b29ed
FuminoOozora <susantoelvis@gmail.com>
2023-04-19 11:37:53 +0700

    Initial Commit

---
+++ Kel5Mini1/Kel5Mini1App.swift
@@ -0,0 +1,20 @@
+//
+//  Kel5Mini1App.swift
+//  Kel5Mini1
+//
+//  Created by Elvis Susanto on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Kel5Mini1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
